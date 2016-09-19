989d9b6cf86ba7ca45c9cb54ca1f656b050dd262
andre <andre.formento@concrete.com.br>
2016-09-18 21:56:06 -0300

    add sonarqube 4.3.2

---
+++ sonarqube/4.3.2/docker-entrypoint.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -e
+
+if [ "$1" = 'sonar' ]; then
+  if [ ! -e ${SONARQUBE_HOME}/conf/sonar.properties.default ]; then
+    cp ${SONARQUBE_HOME}/conf/sonar.properties ${SONARQUBE_HOME}/conf/sonar.properties.default
+
+    # Variables
+    SONAR_JDBC_URL=${SONAR_JDBC_URL:-jdbc:h2:tcp://localhost:9092/sonar}
+    SONAR_JDBC_USERNAME=${SONAR_JDBC_USERNAME:-sonar}
+    SONAR_JDBC_PASSWORD=${SONAR_JDBC_PASSWORD:-sonar}
+
+    # Setting the access to the Database
+    sed -i 's|^sonar.jdbc.username=sonar|sonar.jdbc.username:'"${SONAR_JDBC_USERNAME}"'|g' ${SONARQUBE_HOME}/conf/sonar.properties
+    sed -i 's|^sonar.jdbc.password=sonar|sonar.jdbc.password:'"${SONAR_JDBC_PASSWORD}"'|g' ${SONARQUBE_HOME}/conf/sonar.properties
+    sed -i 's|^sonar.jdbc.url=jdbc:h2:tcp://localhost:9092/sonar|sonar.jdbc.url='"${SONAR_JDBC_URL}"'|g' ${SONARQUBE_HOME}/conf/sonar.properties
+
+    if [ -d /docker-entrypoint-init.d ]; then
+      for f in /docker-entrypoint-init.d/*.sh; do
+        [ -f "$f" ] && . "$f"
+      done
+    fi
+  fi
+
+  exec ${SONARQUBE_HOME}/bin/linux-x86-64/sonar.sh console
+fi
+
+exec "$@"
